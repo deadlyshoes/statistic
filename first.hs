a7ace6ff542bf3a4d84198e072436a453dd37190
deadlyshoes <36314990+deadlyshoes@users.noreply.github.com>
2018-12-26 00:22:26 -0300

    Added range

--- first.hs
+++ first.hs
@@ -1,5 +1,7 @@
 import trivialFuncs
 
+-- Always assume that all functions are in rol
+
 mean :: [Float] -> Float
 mean x = sumList x / sizeList x
 
@@ -7,3 +9,9 @@ median :: [Float] -> Float
 median x
 	| restDiv (sizeList x) 2 == 0 = (elementI x [] ((sizeList x) / 2 - 1) + elementI x [] ((sizeList x) / 2)) / 2
 	| otherwise = elementI x [] (toInt (sizeList x / 2))
+	
+-- mode :: -> 
+-- mode  = 
+
+range :: [Float] -> Float
+range (x:xs) = lastElement xs - x
