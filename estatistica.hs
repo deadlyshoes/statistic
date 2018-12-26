d978c7f90d1250671b9337e4bcf6e45bf7a176b7
deadlyshoes <36314990+deadlyshoes@users.noreply.github.com>
2018-12-26 00:08:04 -0300

    Update estatistica.hs

--- estatistica.hs
+++ estatistica.hs
@@ -29,4 +29,4 @@ mean x = sumList x / sizeList x
 median :: [Float] -> Float
 median x
 	| restDiv (sizeList x) 2 == 0 = (elementI x [] ((sizeList x) / 2 - 1) + elementI x [] ((sizeList x) / 2)) / 2
-	| otherwise = elementI x [] (toInt sizeList x / 2))
+	| otherwise = elementI x [] (toInt (sizeList x / 2))
