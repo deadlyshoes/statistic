ee97175d4dd07639fd3828253673e2ae5efff0b3
hildemar <hildemar@airmail.cc>
2019-08-13 22:54:07 -0300

    calculate integral with riemann sum

--- TrivialFuncs.hs
+++ TrivialFuncs.hs
@@ -53,6 +53,35 @@ repeatAll :: [Float] -> [Int]
 repeatAll [] = []
 repeatAll (x:xs) = repeatTimes x xs:repeatAll xs
 
+--integral adapted from https://rextester.com/KHC32406
+riemanBlock :: (Double, Double) -> (Double -> Double) -> Double
+riemanBlock (a, b) f
+  | a > b     = riemanBlock (b, a) f
+  | otherwise       = ((f a) + (f b)) / 2.0 * (b - a)
+
+riemanIterator :: [Double] -> (Double -> Double) -> [Double]
+riemanIterator [a] _ = []
+riemanIterator (p:q:ps) f = (riemanBlock (p, q) f) : (riemanIterator (q:ps) f)
+
+riemanSum :: [Double] -> (Double -> Double) -> Double
+riemanSum partition f = sum (riemanIterator partition f)
+
+createPartition :: (Double, Double) -> Double -> [Double]
+createPartition (a, b) n
+  | a > b = createPartition (b, a) n
+  | otherwise = [a, b + (b - a)/n .. b]
+
+integralIterator :: Double -> Double -> (Double, Double) -> (Double -> Double) -> Double -> Double
+integralIterator lastIntegral lastNum (a, b) f prec
+  | prec > abs (lastIntegral - currentIntegral) = currentIntegral
+  | otherwise = integralIterator currentIntegral currentNum (a, b) f prec
+  where currentNum = lastNum * 2
+        currentIntegral = riemanSum (createPartition (a, b) currentNum) f
+
+integral :: (Double, Double) -> (Double -> Double) -> Double
+integral (a, b) f = integralIterator (riemanSum [a, b] f) 1 (a, b) f prec
+  where prec = 0.000000001
+
 --input
 firstArgument :: String -> String
 firstArgument (' ':xs) = []
@@ -61,3 +90,4 @@ firstArgument (x:xs) = x:firstArgument xs
 secondArgument :: String -> [Float]
 secondArgument (' ':xs) = read xs :: [Float]
 secondArgument (x:xs) = secondArgument xs
+
